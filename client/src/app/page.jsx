c32b7aaaf2c1d66d170f30615005862dadb0cc39
JatinVermaJv <jatinverma5868@gmail.com>
2025-04-14 13:09:49 +0530

    Initial commit

---
+++ client/src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold flex justify-center items-center h-screen">Originator</h1>
+    </div>
+  );
+}
